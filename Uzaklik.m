b120f41721c989213a4614e9f6f766842dc720fe
prederyo <ifkaraca14@gmail.com>
2025-12-12 14:28:27 +0300

    12.12.2025 ders

---
+++ Uzaklik.m
@@ -0,0 +1,2 @@
+function mesafe=Uzaklik (x1,y1,x2,y2)
+mesafe=sqrt((x2-x1)^2+(y2-y1)^2);
---
+++ Uzaklik1.m
@@ -0,0 +1,6 @@
+x1 = input("x1: ");
+y1 = input("y1: ");
+x2 = input("x2: ");
+y2 = input("y2: ");
+mesafe=sqrt((x2-x1)^2+(y2-y1)^2);
+disp(mesafe);
